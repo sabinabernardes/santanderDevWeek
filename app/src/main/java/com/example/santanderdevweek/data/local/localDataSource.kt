41c107a61d77ae617e8ccb1988f29016dcfca54c
sabinabernardes <sabina.ricardok@gmail.com>
2021-06-12 20:03:10 -0300

    ajusting code

--- app/src/main/java/com/example/santanderdevweek/data/local/localDataSource.kt
+++ app/src/main/java/com/example/santanderdevweek/data/local/localDataSource.kt
@@ -5,15 +5,14 @@ import com.example.santanderdevweek.data.model.Cliente
 import com.example.santanderdevweek.data.model.Conta
 
 class localDataSource {
-    fun getLocalData(): List<Conta> {
+    fun getLocalData() {
         val cliente = Cliente("Sabina")
         val cartao = Cartao("418876645564")
-        return List(
-            "45454545", "252522", "4554545,55", "55457454", cliente, cartao
+        return List("45454545", "252522", "4554545,55", "55457454", cliente, cartao
         )
     }
 
-    private fun List(size: String, init: String, s: String, s1: String, cliente: Cliente, cartao: Cartao): List<Conta> {
+    private fun List(size: String, init: String, s: String, s1: String, cliente: Cliente, cartao: Cartao) {
 
     }
 }
